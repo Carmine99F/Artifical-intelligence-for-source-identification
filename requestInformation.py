6a6bb08eecb6df2848f19336691de9d7f19c03fb
Carmine <Fierroka@outlook.it>
2022-07-06 09:11:43 +0200

    update

---
+++ requestInformation.py
@@ -0,0 +1,94 @@
+import requests
+import json
+
+def getInfo(name):
+    url="https://square.sensesquare.eu:5001/download"
+    arrayInfoCentraline=[]
+    #arrayCentraline=['ITCAMMON134567','ITCAMMON234567','ITCAMMON334567','ITCAMMON444567']
+    for i in range(23):
+        dati={
+            "apikey":"4BSFRYMNPZ0J",
+            "req_type":"hourly",
+            "zoom":5,
+            "start_hour":i,
+            "end_hour":i,
+            "format":"json",
+            "fonti":"ssq",
+            "req_centr":"ITCAMMON134567",
+            "start_date":"2022-03-27",
+            "end_date":"2022-03-27"
+        }
+        response=requests.post(url,data=dati)
+        infoJson=json.loads(str(response.text))
+        #print(infoJson)
+        #print(infoJson["ID"])
+        if float(infoJson["pm10"]) > 50:
+            dataJson={
+                "direzione_vento":float(infoJson["direzione_vento"]),
+                "gas_resistance":float(infoJson["gas_resistance"]),
+                "intensita_vento":float(infoJson["intensita_vento"]),
+                "pm10":float(infoJson["pm10"]),
+                "pm2_5":float(infoJson["pm2_5"])
+            }
+            arrayInfoCentraline.append(dataJson)
+    return arrayInfoCentraline
+    
+
+#print(arrayInfoCentraline)
+# print(len(arrayInfoCentraline))
+# print(type(arrayInfoCentraline))
+# print(arrayInfoCentraline)
+# strInfo="{"+str(arrayInfoCentraline)+"}"
+# strInfo = strInfo.replace("\'", "\"")
+
+# print(strInfo)
+# try:
+#     jsonInfo=json.loads(strInfo)
+#     print(jsonInfo)
+# except Exception as ex:
+#     print(str(ex))
+    
+    
+    
+    
+# dati={
+#     "apikey":"4BSFRYMNPZ0J",
+#     "req_type":"hourly",
+#     "zoom":5,
+#     "start_hour":0,
+#     "end_hour":0,
+#     "format":"json",
+#     "fonti":"ssq",
+#     "req_centr":"ITCAMMON134567",
+#     "start_date":"2022-03-27",
+#     "end_date":"2022-03-27"
+# }
+# response=requests.post(url,data=dati)
+# print(response.text)
+
+
+#print(type(response.json()))
+#print(response.text)
+#print(json.dumps(response.json(), indent=4))
+
+#jsonInfo=json.dumps((response.text))
+#print(response.json())
+
+# try:
+#     infoJson=json.loads(str(response.text))
+#     print(infoJson)
+#     print(infoJson["ID"])
+#     dataJson={
+#         "direzione_vento":float(infoJson["direzione_vento"]),
+#         "gas_resistance":float(infoJson["gas_resistance"]),
+#         "intensita_vento":float(infoJson["intensita_vento"]),
+#         "pm10":float(infoJson["pm10"]),
+#         "pm2_5":float(infoJson["pm2_5"])
+#     }
+#     print(json.dumps(dataJson,indent=4))
+#     print(type(json.dumps(dataJson)))
+    
+    
+# except Exception as ex:
+#     print(str(ex))
+# #print(infoJson)
--- traingulationMain.py
+++ traingulationMain.py
@@ -1,14 +1,90 @@
 import json
 from textwrap import indent
+from urllib import response
 import triangulationModule as tm 
-import geojson 
+import geojson
+import requests
+import time 
+import requestInformation
+
+# url="https://square.sensesquare.eu:5001/download"
+# dati={
+#     "apikey":"4BSFRYMNPZ0J",
+#     "req_type":"hourly",
+#     "zoom":5,
+#     "start_hour":0,
+#     "end_hour":23,
+#     "format":"json",
+#     "fonti":"ssq",
+#     "req_centr":"ITCAMMON134567",
+#     "start_date":"2022-03-27",
+#     "end_date":"2022-03-27"
+# }
+
+# response=requests.post(url,data=dati)
+# print(response.status_code)
+# print(response.text)
+# time.sleep(3000)
+
+
+arrayCentraline=['ITCAMMON134567','ITCAMMON234567','ITCAMMON334567','ITCAMMON444567']
+infoCentraline={
+    "ITCAMMON134567":{
+        "lat":40.821009,
+        "lon":14.810876
+    },
+    "ITCAMMON234567":{  
+        "lat": 40.828705,
+        "lon": 14.801567
+    },
+    "ITCAMMON334567":{
+        "lat": 40.828199,
+        "lon": 14.809813
+    },
+    "ITCAMMON444567":{
+        "lat": 40.824999,
+        "lon": 14.819923
+    }
+}
+geovuoto={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            
+          ]
+        ]
+      }
+    }
+  ]
+}
+
+for item in arrayCentraline:
+
+    #ci prendiamo lat e lon dal backend per ogni sensore
+    arraycoo=[infoCentraline[item]["lon"],infoCentraline[item]["lat"]]
+    geovuoto["features"][0]['geometry']["coordinates"][0].append(arraycoo)
+    
+geovuoto["features"][0]['geometry']["coordinates"][0].append(geovuoto["features"][0]['geometry']["coordinates"][0][0])
+print(geovuoto)
 
 directory="coordinates/"
-filename="giffoni.geojson"
+filename="cosenza.geojson"
 locationFile=directory+""+filename
-with open(locationFile,"r") as f:
-    jsonCoordinates=json.load(f)
-    
+
+# with open(locationFile,"r") as f:
+#     jsonCoordinates=json.load(f)
+jsonCoordinates=geovuoto  
+
+with open("testauto.geojson","w") as fp:
+    json.dump(jsonCoordinates,fp)
+      
 features=(jsonCoordinates["features"][0])
 geometry=features["geometry"]
 coordinates=geometry["coordinates"][0]
@@ -36,7 +112,6 @@ if listTraingles is not None:
     try:
         with open(outputfile,'w') as f:
             f.write(str(geoJsonObject.__str__().replace("'", '"')))
-            #f.close()
     except Exception as ex:
         print(str(ex))
 else:
